09237039584d3f00ba468405d46a1fd752a71ad4
Carlos Monteiro <carlos.monteiro@cybernet.local>
2020-10-15 11:12:44 -0300

    Realizando alinhamentos de back-end para o modal

--- view/admin/adminview.php
+++ view/admin/adminview.php
@@ -43,6 +43,7 @@ $user_check=$_SESSION['usuarioUser'];
                 <table class="grid-16 tabela">
                   
                     <tr>
+                    <th>Detalhes</th>
                     <th>Provedor</th>
                     <th>Responsavel</th>
                     <th>Protocolo</th> 
@@ -50,9 +51,6 @@ $user_check=$_SESSION['usuarioUser'];
                     <th>Assunto</th>
                     <th>Status</th> 
                     <th>Solicitação</th>
-                    <th>Ações
-
-                    </th>
                     </tr>
                 
                     <?php 
@@ -60,8 +58,9 @@ $user_check=$_SESSION['usuarioUser'];
                     FROM tb_chamados";
                     $con = $mysqli->query($lista_colaboradores) or die ($mysqli->error);
                     while($dados = $con->fetch_array()){ ?>
-                   
+                    
                     <tr>
+                    <td><a href="#" class="<?php echo date('dmY', strtotime($dados['data_solicitacao']))?><?php echo $dados['cod'];?>"><img src="../../img/eye.png" alt=""></a></td>    
                     <td><?php echo $dados['provedor'];?></td>
                     <td><?php echo $dados['responsavel'];?></td>
                     <td><?php echo date('dmY', strtotime($dados['data_solicitacao']))?>-<?php echo $dados['cod'];?></td>
@@ -69,7 +68,6 @@ $user_check=$_SESSION['usuarioUser'];
                     <td><?php echo $dados['assunto'];?></td>
                     <td><?php echo $dados['status_chamado'];?></td>
                     <td><?php echo date('d/m/Y', strtotime($dados['data_solicitacao']));?></td>
-                    <td> <i class="<?php echo $dados['responsavel'];?><?php echo $dados['cod'];?>"><img src="../../img/eye.png" alt=""></i></td>
                     </tr>
                     <?php } ?>
             </table>
@@ -84,14 +82,18 @@ $user_check=$_SESSION['usuarioUser'];
     <div id="myModal" class="modal">
     <div class="modal-content container">
     <i class="close grid-16">&times;</i>
-
+            
+            <div class="titulo_modal grid-16 form_ind">
+            <h1>Detalhamento do chamado:</h1>
+            </div>
+            
             <div class="grid-6 form_ind">
-            <label for="fname">Empresa:</label><br>    
+            <label for="fname">Provedor:</label><br>    
             <input type="text" name="grava_provedor_empresa" id="grava_provedor_empresa" onkeyup="maiuscula(this)"/>
             </div>
 
             <div class="grid-6 form_ind">
-            <label for="fname">CNPJ:</label><br>    
+            <label for="fname">Solicitante:</label><br>    
             <input type="text" name="grava_provedor_cnpj" id="grava_provedor_cnpj">
             </div>
 
@@ -101,23 +103,36 @@ $user_check=$_SESSION['usuarioUser'];
             </div> 
 
             <div class="grid-6 form_ind">
-            <label for="fname">Empresa:</label><br>    
+            <label for="fname">Protocolo:</label><br>    
             <input type="text" name="grava_provedor_empresa" id="grava_provedor_empresa" onkeyup="maiuscula(this)"/>
             </div>
 
             <div class="grid-6 form_ind">
-            <label for="fname">CNPJ:</label><br>    
+            <label for="fname">Prioridade:</label><br>    
             <input type="text" name="grava_provedor_cnpj" id="grava_provedor_cnpj">
             </div>
 
             <div class="grid-4 form_ind">
-            <label for="fname">E-mail:</label><br>    
+            <label for="fname">Data de solicitação:</label><br>    
             <input type="text" name="grava_provedor_email" id="grava_provedor_email" maxlength="50">
             </div> 
 
+            <div class="grid-8 form_ind">
+            <label for="fname">Status:</label><br>    
+            <input type="text" name="grava_provedor_cnpj" id="grava_provedor_cnpj">
+            </div>
+
+            <div class="grid-8 form_ind">
+            <label for="fname">Assunto:</label><br>    
+            <input type="text" name="grava_provedor_cnpj" id="grava_provedor_cnpj">
+            </div>
+
+            <div class="grid-16 form_ind">
+            <label for="fname">Mensagem:</label><br>    
+            <input type="text" name="grava_provedor_cnpj" id="grava_provedor_cnpj">
+            </div>
      </div>
     </div>
-
 
 <?php 
 $lista_colaboradores = "SELECT *
@@ -132,7 +147,7 @@ var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 
-var btn = document.getElementsByClassName("<?php echo $dados['responsavel'];?><?php echo $dados['cod'];?>")[0];
+var btn = document.getElementsByClassName("<?php echo date('dmY', strtotime($dados['data_solicitacao']))?><?php echo $dados['cod'];?>")[0];
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -156,6 +171,52 @@ modal.style.display = "none";
 
 </script>
 <?php } ?>
+
+
+<script>
+
+function editar(e) {
+  var linha = $(e).closest("tr");
+  var id = linha.find("td:eq(0)").text().trim();
+  var nome = linha.find("td:eq(1)").text().trim();
+  var cpf = linha.find("td:eq(7)").text().trim();
+  var rg = linha.find("td:eq(8)").text().trim();
+  var nascimento = linha.find("td:eq(9)").text().trim();
+  var admissao = linha.find("td:eq(10)").text().trim();
+  var sexo = linha.find("td:eq(11)").text().trim();
+  var telefone = linha.find("td:eq(12)").text().trim();
+  var celular = linha.find("td:eq(2)").text().trim();
+  var email = linha.find("td:eq(3)").text().trim();
+  var condicao = linha.find("td:eq(5)").text().trim();
+  var cep = linha.find("td:eq(13)").text().trim();
+  var rua = linha.find("td:eq(14)").text().trim();
+  var bairro = linha.find("td:eq(15)").text().trim();
+  var numero = linha.find("td:eq(16)").text().trim();
+  var cidade = linha.find("td:eq(17)").text().trim();
+  var setor = linha.find("td:eq(18)").text().trim();
+  var cargo = linha.find("td:eq(4)").text().trim();
+  var salario = linha.find("td:eq(19)").text().trim();
+  
+  
+  $("#modalId").val(id);
+  $("#edita_colaborador_nome").val(nome);
+  $("#edita_colaborador_cpf").val(cpf);
+  $("#edita_colaborador_rg").val(rg);
+  $("#edita_colaborador_nascimento").val(nascimento);
+  $("#edita_colaborador_admissao").val(admissao);
+  $("#edita_colaborador_sexo").val(sexo);
+  $("#edita_colaborador_telefone").val(telefone);
+  $("#edita_colaborador_celular").val(celular);
+  $("#edita_colaborador_email").val(email);
+  $("#edita_colaborador_condicao").val(condicao);
+  $("#edita_colaborador_cep").val(cep);
+  $("#edita_colaborador_rua").val(rua);
+  $("#edita_colaborador_bairro").val(bairro);
+  $("#edita_colaborador_numero").val(numero);
+  $("#edita_colaborador_cidade").val(cidade);
+}
+</script>
+
 
 <footer>
         <div class="container">
