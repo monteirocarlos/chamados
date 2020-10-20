f4e80a30453c52ac31199e3ebfa23d24433418b4
Carlos Monteiro <carlos.monteiro@cybernet.local>
2020-10-20 16:16:40 -0300

    Modal final v1.0 concluído

--- view/admin/adminview.php
+++ view/admin/adminview.php
@@ -11,6 +11,7 @@ $user_check=$_SESSION['usuarioUser'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
 
     <title>Meus Chamados - Cybernet</title>
 </head>
@@ -29,67 +30,33 @@ $user_check=$_SESSION['usuarioUser'];
         </div>
     </header>
 
-    <section>
+<section>
         <div class="container">
             <div class="titulo">
-            <h1>VISUALIZAR CHAMADOS</h1>
+                <h1>VISUALIZAR CHAMADOS</h1>
+            </div>
         </div>
-        </div>
-    </section>
+</section>
    
-    <section>
-        <div class="tabela_geral">
+<section>
+    <div id="modal">
+		<div class="container modal-content">
+			
+			<div>
+            <form action="">
             
-                <table class="grid-16 tabela">
-                  
-                    <tr>
-                    <th>Detalhes</th>
-                    <th>Provedor</th>
-                    <th>Responsavel</th>
-                    <th>Protocolo</th> 
-                    <th>Prioridade</th>
-                    <th>Assunto</th>
-                    <th>Status</th> 
-                    <th>Solicitação</th>
-                    </tr>
-                
-                    <?php 
-                    $lista_colaboradores = "SELECT * 
-                    FROM tb_chamados";
-                    $con = $mysqli->query($lista_colaboradores) or die ($mysqli->error);
-                    while($dados = $con->fetch_array()){ ?>
-                    
-                    <tr>
-                    <td><a href="#" class="<?php echo date('dmY', strtotime($dados['data_solicitacao']))?><?php echo $dados['cod'];?>"><img src="../../img/eye.png" alt=""></a></td>    
-                    <td><?php echo $dados['provedor'];?></td>
-                    <td><?php echo $dados['responsavel'];?></td>
-                    <td><?php echo date('dmY', strtotime($dados['data_solicitacao']))?>-<?php echo $dados['cod'];?></td>
-                    <td><?php echo $dados['prioridade'];?></td>
-                    <td><?php echo $dados['assunto'];?></td>
-                    <td><?php echo $dados['status_chamado'];?></td>
-                    <td><?php echo date('d/m/Y', strtotime($dados['data_solicitacao']));?></td>
-                    </tr>
-                    <?php } ?>
-            </table>
-            
-        </div>
-    </section>
+            <div class="grid-16 cf">
+                <a class= "form_ind" href="#"><i ><img src="../../img/close.png" alt=""></i></a>
+            </div>
 
-    <div class="container">
-        
-    </div>
-  
-    <div id="myModal" class="modal">
-    <div class="modal-content container">
-    <i class="close grid-16">&times;</i>
-            
             <div class="titulo_modal grid-16 form_ind">
-            <h1>Detalhamento do chamado:</h1>
+            <h1>Detalhamento do chamado:</h1>	
             </div>
             
             <div class="grid-6 form_ind">
             <label for="fname">Provedor:</label><br>    
-            <input type="text" name="view_provedor" id="view_provedor" onkeyup="maiuscula(this)"/>
+            <input type="text" name="view_provedor" id="view_provedor" value="<?php echo $dados['provedor'];?>"/>
+            <input type="hidden"  class="form-control" name="modalId" id="modalId"/>
             </div>
 
             <div class="grid-6 form_ind">
@@ -131,91 +98,53 @@ $user_check=$_SESSION['usuarioUser'];
             <label for="fname">Mensagem:</label><br>    
             <input type="text" name="view_mensagem" id="view_mensagem">
             </div>
-     </div>
-    </div>
 
-<?php 
-$lista_colaboradores = "SELECT *
-FROM tb_chamados";
-$con = $mysqli->query($lista_colaboradores) or die ($mysqli->error);
-while($dados = $con->fetch_array()){ ?>
+            </form>
+			</div>
 
-<script>
+			
+		</div>
+		<div class="overlay"></div>
+	</div>  
+</section>
 
-// Get the modal
-var modal = document.getElementById("myModal");
+<section>
+        <div class="tabela_geral">
 
-// Get the button that opens the modal
+                <table class="grid-16 tabela">
+                  
+                    <tr>
+                    <th>Detalhes</th>
+                    <th>Provedor</th>
+                    <th>Responsavel</th>
+                    <th>Protocolo</th> 
+                    <th>Prioridade</th>
+                    <th>Assunto</th>
+                    <th>Status</th> 
+                    <th>Solicitação</th>
+                    </tr>
 
-var btn = document.getElementsByClassName("<?php echo date('dmY', strtotime($dados['data_solicitacao']))?><?php echo $dados['cod'];?>")[0];
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-if (event.target == modal) {
-modal.style.display = "none";
-}
-}
-
-</script>
-<?php } ?>
-
-
-<script>
-
-function editar(e) {
-  var linha = $(e).closest("tr");
-  var id = linha.find("td:eq(0)").text().trim();
-  var nome = linha.find("td:eq(1)").text().trim();
-  var cpf = linha.find("td:eq(7)").text().trim();
-  var rg = linha.find("td:eq(8)").text().trim();
-  var nascimento = linha.find("td:eq(9)").text().trim();
-  var admissao = linha.find("td:eq(10)").text().trim();
-  var sexo = linha.find("td:eq(11)").text().trim();
-  var telefone = linha.find("td:eq(12)").text().trim();
-  var celular = linha.find("td:eq(2)").text().trim();
-  var email = linha.find("td:eq(3)").text().trim();
-  var condicao = linha.find("td:eq(5)").text().trim();
-  var cep = linha.find("td:eq(13)").text().trim();
-  var rua = linha.find("td:eq(14)").text().trim();
-  var bairro = linha.find("td:eq(15)").text().trim();
-  var numero = linha.find("td:eq(16)").text().trim();
-  var cidade = linha.find("td:eq(17)").text().trim();
-  var setor = linha.find("td:eq(18)").text().trim();
-  var cargo = linha.find("td:eq(4)").text().trim();
-  var salario = linha.find("td:eq(19)").text().trim();
-  
-  
-  $("#modalId").val(id);
-  $("#edita_colaborador_nome").val(nome);
-  $("#edita_colaborador_cpf").val(cpf);
-  $("#edita_colaborador_rg").val(rg);
-  $("#edita_colaborador_nascimento").val(nascimento);
-  $("#edita_colaborador_admissao").val(admissao);
-  $("#edita_colaborador_sexo").val(sexo);
-  $("#edita_colaborador_telefone").val(telefone);
-  $("#edita_colaborador_celular").val(celular);
-  $("#edita_colaborador_email").val(email);
-  $("#edita_colaborador_condicao").val(condicao);
-  $("#edita_colaborador_cep").val(cep);
-  $("#edita_colaborador_rua").val(rua);
-  $("#edita_colaborador_bairro").val(bairro);
-  $("#edita_colaborador_numero").val(numero);
-  $("#edita_colaborador_cidade").val(cidade);
-}
-</script>
+                    <?php 
+                    $lista_colaboradores = "SELECT *
+                    FROM tb_chamados";
+                    $con = $mysqli->query($lista_colaboradores) or die ($mysqli->error);
+                    while($dados = $con->fetch_array()){ ?>
+                    
+                    <tr>
+                    <td><i class="<?php echo date('dmY', strtotime($dados['data_solicitacao']))?><?php echo $dados['cod'];?>" onclick="editar(this)"><p><a href="#modal" class="btn_modal"><img src="../../img/eye.png" alt=""></a></p></i></td>
+                    <td><?php echo $dados['provedor'];?></td>
+                    <td><?php echo $dados['responsavel'];?></td>
+                    <td><?php echo date('dmY', strtotime($dados['data_solicitacao']))?>-<?php echo $dados['cod'];?></td>
+                    <td><?php echo $dados['prioridade'];?></td>
+                    <td><?php echo $dados['assunto'];?></td>
+                    <td><?php echo $dados['status_chamado'];?></td>
+                    <td><?php echo date('d/m/Y', strtotime($dados['data_solicitacao']));?></td>
+                    </tr>
+                    <?php } ?>
+            </table>
+            
+        </div>
+</section>
 
 <footer>
         <div class="container">
@@ -224,6 +153,21 @@ function editar(e) {
         </div>
         </div>
 </footer>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+            
+            <script>
+            function editar(e) {
+            var linha = $(e).closest("tr");
+            var id = linha.find("td:eq(0)").text().trim();
+            var provedor = linha.find("td:eq(2)").text().trim();
+            var solicitante = linha.find("td:eq(3)").text().trim(); 
+        
+            $("#modalId").val(id);    
+            $("#view_provedor").val(provedor);
+            $("#view_solicitante").val(solicitante);
+            }
+</script>
+
 </body>
 </html>
- 
